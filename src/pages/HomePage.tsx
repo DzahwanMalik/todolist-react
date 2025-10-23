66b0a3556864529948ba2a9918b06ed798a2042a
DzahwanMalik <malikdzahwan22@gmail.com>
2025-10-24 00:05:29 +0700

    fix: re fix bug

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,11 @@
+import TaskContainer from "../components/common/molecules/TaskContainer/TaskContainer";
+
 const HomePage = () => {
-  return <div></div>;
+  return (
+    <div className="w-full">
+      <TaskContainer />
+    </div>
+  );
 };
 
 export default HomePage;
